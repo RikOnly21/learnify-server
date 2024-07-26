d86de3991b3eb63a1be3e2655cb881fe0320181f
Rik <nhocngusi2003@gmail.com>
2024-07-26 21:23:40 +0700

    yes3

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,19 @@ import { createClerkClient } from "@clerk/backend";
 import { createOpenAI } from "@ai-sdk/openai";
 import { generateText } from "ai";
 
-import { prisma } from "../src/db/db";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+	return new PrismaClient();
+};
+
+declare const globalThis: {
+	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
 type Env = {
 	Variables: {
 		userId: string | undefined;
