a7387020bc53cd623c054414f4fb96d13e51bea2
Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2024-08-03 06:03:00 +0700

    refactor: simplify server code by using hono var, create user if not exist in database.

--- api/index.ts
+++ api/index.ts
@@ -1,78 +1,72 @@
 import { handle } from "@hono/node-server/vercel";
 import { Hono } from "hono";
 
-import { waitUntil } from "@vercel/functions";
+import { createClerkClient, type ClerkClient } from "@clerk/backend";
 
-import { createClerkClient } from "@clerk/backend";
-
-import { z } from "zod";
-import { createOpenAI } from "@ai-sdk/openai";
+import { createOpenAI, type OpenAIProvider } from "@ai-sdk/openai";
 import { generateObject, generateText } from "ai";
+import { z } from "zod";
 
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-	return new PrismaClient();
-};
+const getPrismaClient = () => new PrismaClient();
 
-declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare const globalThis: { prismaGlobal: ReturnType<typeof getPrismaClient> } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalThis.prismaGlobal ?? getPrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 type Env = {
 	Variables: {
 		userId: string | undefined;
+		openai: OpenAIProvider;
+		clerk: ClerkClient;
 	};
 };
 
-export const config = {
-	api: {
-		bodyParser: false,
-	},
-};
+export const config = { api: { bodyParser: false } };
 
 const app = new Hono<Env>().basePath("/api");
 
-app.get("/", (c) => c.json({ message: "test" }));
+app.get("/", (c) => c.json({ message: "Server Working!!!" }));
 
 app.use("/user/*", async (c, next) => {
 	const userId = c.req.header()["clerk-user-id"];
 	if (!userId) return c.json({ error: { message: "Unauthorized" } }, 401);
 
+	const openai = createOpenAI({ apiKey: process.env.API_KEY, baseURL: process.env.API_URL });
+
+	const clerk = createClerkClient({
+		secretKey: process.env["CLERK_SECRET_KEY"],
+		publishableKey: process.env["CLERK_PUBLISHABLE_KEY"],
+	});
+
+	const isUserExist = await prisma.user.findUnique({ where: { userId } });
+	if (!isUserExist) {
+		const user = await clerk.users.getUser(userId);
+		await prisma.user.create({ data: { name: user.username, userId, imageUrl: user.imageUrl } });
+	}
+
 	c.set("userId", userId);
+	c.set("openai", openai);
+	c.set("clerk", clerk);
+
 	return await next();
 });
 
 app.get("/user/messages", async (c) => {
 	const userId = c.var.userId!;
 
-	const client = createClerkClient({
-		secretKey: process.env["CLERK_SECRET_KEY"],
-		publishableKey: process.env["CLERK_PUBLISHABLE_KEY"],
-	});
-
-	const user = await client.users.getUser(userId);
+	const user = await c.var.clerk.users.getUser(userId);
 	const messages = await prisma.message.findMany({ where: { userId } });
 
 	return c.json({ user, messages });
 });
 
 app.post("/user/voice", async (c) => {
-	const userId = c.var.userId!;
 	const messages = (await c.req.json()) as { content: string; role: "user" | "assistant" }[];
 
-	const openai = createOpenAI({
-		apiKey: process.env.API_KEY,
-		baseURL: process.env.API_URL,
-	});
-
-	const { text } = await generateText({
-		model: openai("gpt-4o"),
-		messages,
-	});
+	const { text } = await generateText({ model: c.var.openai("gpt-4o"), messages });
 
 	return c.json({ message: text });
 });
@@ -81,15 +75,7 @@ app.post("/user/messages/create", async (c) => {
 	const userId = c.var.userId!;
 	const messages = (await c.req.json()) as { content: string; role: "user" | "assistant" }[];
 
-	const openai = createOpenAI({
-		apiKey: process.env.API_KEY,
-		baseURL: process.env.API_URL,
-	});
-
-	const { text } = await generateText({
-		model: openai("gpt-4o"),
-		messages,
-	});
+	const { text } = await generateText({ model: c.var.openai("gpt-4o"), messages });
 
 	const userMessage = messages.pop()!;
 	await prisma.message.createMany({
@@ -116,13 +102,8 @@ app.post("/user/questions/start", async (c) => {
 
 	const question = `Give me ${numOfQuestion} questions about ${subject}, all questions must be unique and the level should be ${difficulty}`;
 
-	const openai = createOpenAI({
-		apiKey: process.env.API_KEY,
-		baseURL: process.env.API_URL,
-	});
-
 	const { object } = await generateObject({
-		model: openai("gpt-4o"),
+		model: c.var.openai("gpt-4o"),
 		prompt: question,
 		temperature: 0.8,
 		presencePenalty: 0.02,
